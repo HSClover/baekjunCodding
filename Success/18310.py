42412b825217af435edfade58a7f18309f13621c
CHOI DONGHYEOK <kkej1009@naver.com>
2025-12-24 00:29:24 +0900

    충돌 수정

--- Success/18310.py
+++ Success/18310.py
@@ -3,21 +3,6 @@ import sys
 N = int(sys.stdin.readline().strip())
 houses = list(map(int,sys.stdin.readline().split()))
 
-<<<<<<< HEAD
-=======
-# min_distance = int(1e9)
-# min_house = houses[0]
-    
-# for i in houses:
-#     distance = 0
-#     for j in houses:
-#             distance += abs(i - j)
-#     min_distance = min(min_distance, distance)
-#     min_house = i if min_distance == distance else min_house
-
-# print(min_house)
-
->>>>>>> f5a444c0d221001b241da4f7cf1d6cc94c9ac91d
 houses.sort()
 if N % 2 == 1:
     print(houses[N // 2])
